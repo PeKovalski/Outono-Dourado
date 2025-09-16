1bef513f6a9a971b8c4a30d4e71ff8690c72ca62
IzabelBacon <168193034+IzabelBacon@users.noreply.github.com>
2025-09-16 08:23:58 -0300

    coloquei o png de abobora

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ const PRODUCTS_DATABASE = [
         description: "Abobrinhas orgânicas da variedade italiana, tenras e saborosas com casca comestível. Baixas em calorias e versáteis na cozinha. Colhidas no ponto ideal.",
         price: 7.90,
         category: "legumes",
-        image: "https://images.unsplash.com/photo-1566385101042-1a0aa0c1268c?ixlib=rb-4.0.3&auto=format&fit=crop&w=300&q=80",
+        image: "abobora.png",
         rating: 4.5,
         reviews: 118,
         inStock: true,
